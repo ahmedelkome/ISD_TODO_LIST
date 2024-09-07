61bee98315eaa838fd692304066231cc72e4ca39
ahmedelkomy <ahmedelkomy.2889@gmail.com>
2024-09-07 21:12:06 +0300

    create Constants class for Strings

---
+++ src/main/kotlin/utils/Constants.kt
@@ -0,0 +1,27 @@
+package utils
+
+object Constants {
+
+    const val TODO_LIST = "\nTodo List "
+    const val FILENAME = "Tasks.txt"
+    const val ADD = "1. Add a new task"
+    const val REMOVE = "2. Remove a task"
+    const val MARK_AS_COMPLETED = "3. Mark a task as completed"
+    const val GET_ALL_TASK = "4. Get all tasks"
+    const val CLEAR_ALL = "5. Clear all tasks"
+    const val EXIT = "6. Exit"
+    const val PLEASE_ENTER_DESCRIPTION = "Please Enter Description"
+    const val NO_TASK_AVAILABLE = "No tasks available"
+    const val TASK_NOT_FOUND = "Task not found"
+    const val COMPLETED = "Completed"
+    const val NOT_COMPLETED = "Not Completed"
+    const val FILE_CLEARED = "File cleared."
+    const val FILE_NOT_FOUND = "File not found."
+    const val NO_TASKS_FOUND = "No tasks found."
+    const val ENTER_TASK_DESCRIPTION = "Enter task description: "
+    const val ENTER_TASK_ID = "Enter task ID : "
+    const val INVALID_ID = "Invalid ID"
+    const val TASKS_SAVED_EXITING = "Tasks saved. Exiting..."
+    const val INVALID_OPTION = "Invalid option, please try again."
+    const val TASKS_LOADED_FROM_FILE= "Tasks loaded from file:"
+}
