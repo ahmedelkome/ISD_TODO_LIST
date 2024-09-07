b23cb43fe3ebe715633be909d9a84ba8015c46dd
ahmed elkomy <ahmedelkomy@gmail.com>
2024-09-07 21:16:33 +0300

    Remove Task Implementation
    this class have implementation to remove task and save it in file

---
+++ src/main/kotlin/data/services/remove/RemoveTaskImpl.kt
@@ -0,0 +1,22 @@
+package data.services.remove
+
+import contract.add.remove.RemoveTask
+import model.Task
+import utils.Constants
+import utils.FileOperation
+
+class RemoveTaskImpl (private val fileOperation: FileOperation): RemoveTask {
+    override fun removeTask(id: Int, tasks: MutableList<Task>) {
+
+        val task = tasks.find {
+            it.id == id
+        }
+        if (task != null) {
+            tasks.remove(task)
+            fileOperation.saveTasksToFile(tasks = tasks, filename = Constants.FILENAME)
+            println("Task Removed ${task.description}")
+        } else {
+            println(Constants.TASK_NOT_FOUND)
+        }
+    }
+}
