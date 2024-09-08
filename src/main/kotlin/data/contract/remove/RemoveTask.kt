b9c65b78e7fd78c1c4080d45e78fec393452d981
ahmedelkomy <ahmedelkomy.2889@gmail.com>
2024-09-08 03:45:02 +0300

    refactor Remove Task for remove by description

--- src/main/kotlin/data/contract/remove/RemoveTask.kt
+++ src/main/kotlin/data/contract/remove/RemoveTask.kt
@@ -4,4 +4,6 @@ import model.Task
 
 interface RemoveTask {
     fun removeTask(id: Int, tasks: MutableList<Task>)
+
+    fun removeTaskByDescription(description: String, tasks: MutableList<Task>)
 }
--- src/main/kotlin/data/services/remove/RemoveTaskImpl.kt
+++ src/main/kotlin/data/services/remove/RemoveTaskImpl.kt
@@ -5,7 +5,7 @@ import model.Task
 import utils.Constants
 import utils.FileOperation
 
-class RemoveTaskImpl (private val fileOperation: FileOperation): RemoveTask {
+class RemoveTaskImpl(private val fileOperation: FileOperation) : RemoveTask {
     override fun removeTask(id: Int, tasks: MutableList<Task>) {
 
         val task = tasks.find {
@@ -19,4 +19,16 @@ class RemoveTaskImpl (private val fileOperation: FileOperation): RemoveTask {
             println(Constants.TASK_NOT_FOUND)
         }
     }
+
+    override fun removeTaskByDescription(description: String, tasks: MutableList<Task>) {
+        val task = tasks.find { it.description.lowercase() == description.lowercase() }
+        if (task != null) {
+            tasks.remove(task)
+            fileOperation.saveTasksToFile(tasks = tasks, filename = Constants.FILENAME)
+            println("Task Removed: ${task.description}")
+        } else {
+            println(Constants.TASK_NOT_FOUND)
+        }
+    }
 }
+
